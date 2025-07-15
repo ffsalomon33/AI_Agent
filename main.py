08c36e8ae2e58011a2d262f5ddc2cc17ce76ccc0
ffsalomon33 <ffsalomon@gmail.com>
2025-07-14 23:58:17 -0500

    Adding files

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+
 def main():
-    print("Hello from ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    print(api_key) 
 
 
 if __name__ == "__main__":
