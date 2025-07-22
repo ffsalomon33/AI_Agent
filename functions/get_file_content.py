a915e6343fa6ff9ebd2fd4e1300294a29c7bb6cd
ffsalomon33 <ffsalomon@gmail.com>
2025-07-21 23:21:54 -0500

    Added get_file_content

---
+++ functions/get_file_content.py
@@ -0,0 +1,19 @@
+import os
+from .config import MAX_CHARS
+
+
+def get_file_content(working_directory, file_path):
+    try:
+        working_path = os.path.abspath(working_directory)
+        file_path_full = os.path.join(working_path, file_path)
+        if not file_path_full.startswith(working_path):
+            f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(file_path_full):
+            f'Error: File not found or is not a regular file: "{file_path}"'
+        with open(file_path_full, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+        if os.path.getsize(file_path_full) > 10000:
+            file_content_string += f'[...File "{file_path}" truncated at 10000 characters]'
+        return file_content_string
+    except Exception as e:
+        return f"Error: {str(e)}"
--- tests.py
+++ tests.py
@@ -1,10 +1,17 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 print("Test 1")
-print(get_files_info("calculator", "."))
+#print(get_files_info("calculator", "."))
+#print(get_file_content("calculator", "lorem.txt"))
 print("Test 2")
-print(get_files_info("calculator", "pkg"))
+#print(get_files_info("calculator", "pkg"))
+print(get_file_content("calculator", "main.py"))
 print("Test 3")
-print(get_files_info("calculator", "/bin"))
+#print(get_files_info("calculator", "/bin"))
+print(get_file_content("calculator", "pkg/calculator.py"))
 print("Test 4")
-print(get_files_info("calculator", "../"))
+#print(get_files_info("calculator", "../"))
+print(get_file_content("calculator", "/bin/cat"))
+print("Test 5")
+print(get_file_content("calculator", "pkg/does_not_exist.py"))
